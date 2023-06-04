c168230fb2f41f09312c584460d65af81c704aae
Hudson da Silva Carlos <hudsonscarlos@outlook.com>
2023-06-04 19:21:46 -0300

    feat: Organizando as pastas do projeto, atualizando alguns pacotes.

---
+++ docs/Index.md
@@ -0,0 +1,20 @@
+# Microsservicos Serverless
+=====================
+Esse repositório foi criado para estudos sobre microsserviços serverless com .Net Core e AWS.
+
+## Deixe uma estrela! :star:
+Se o projeto te ajudou ou se achou interessante, deixe uma estrela ;)
+
+## Como usar:
+- Você precisará do Visual Studio 2022 ou mais recentes dependendo do momento que visualizar esta página.
+- É possível também utilizar o VS Code para rodar o projeto.
+
+Segue o link para auxiliar na configuração do ambiente de trabalho: https://dotnet.microsoft.com/pt-br/download
+
+## Technologies implemented:
+
+- .NET Core 3.1
+- AWS-SQS
+- Lambda
+- Function
+- DynamoDB
---
+++ src/Cadastrador/Filters/BasePathFilter.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Cadastrador.Filters
+{
+    /// <summary>
+    /// O filtro de documento BasePath define a propriedade BasePath do Swagger e a remove dos caminhos de URL individuais
+    /// </summary>
+    public class BasePathFilter : IDocumentFilter
+    {
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        /// <param name="basePath">BasePath para remover das operações</param>
+        public BasePathFilter(string basePath)
+        {
+            BasePath = basePath;
+        }
+
+        /// <summary>
+        /// Obtém o BasePath do Swagger Doc
+        /// </summary>
+        /// <returns>O BasePath do Swagger Doc</returns>
+        public string BasePath { get; }
+
+        /// <summary>
+        /// Aplique o filtro
+        /// </summary>
+        /// <param name="swaggerDoc">OpenApiDocument</param>
+        /// <param name="context">FilterContext</param>
+        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+        {
+            swaggerDoc.Servers.Add(new OpenApiServer() { Url = this.BasePath });
+
+            var pathsToModify = swaggerDoc.Paths.Where(p => p.Key.StartsWith(this.BasePath)).ToList();
+
+            foreach (var path in pathsToModify)
+            {
+                if (path.Key.StartsWith(this.BasePath))
+                {
+                    string newKey = Regex.Replace(path.Key, $"^{this.BasePath}", string.Empty);
+                    swaggerDoc.Paths.Remove(path.Key);
+                    swaggerDoc.Paths.Add(newKey, path.Value);
+                }
+            }
+        }
+    }
+}
